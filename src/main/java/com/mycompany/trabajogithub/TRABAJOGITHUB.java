dbf55e74b7386e6b917cac24ef9f4cdc5719d081
unknown <ser.carrasco@duocuc.cl>
2025-04-07 23:23:13 -0400

    primer commit

---
+++ src/main/java/com/mycompany/trabajogithub/TRABAJOGITHUB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajogithub;
+
+/**
+ *
+ * @author Sergio Carrasco
+ */
+public class TRABAJOGITHUB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
